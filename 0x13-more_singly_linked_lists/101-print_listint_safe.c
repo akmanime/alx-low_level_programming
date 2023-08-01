4ecd818cfe21e1219a4c962311b989152cd58a8c
akmanime <akramryad3@gmail.com>
2023-07-31 19:03:59 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -31,31 +31,31 @@ void free_listp(listp_t **head)
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
+size_t nnodes = 0;
+listp_t *hptr, *new, *add;
 
-	hptr = NULL;
-	while (head != NULL)
-	{
-		new = malloc(sizeof(listp_t));
+hptr = NULL;
+while (head != NULL)
+{
+new = malloc(sizeof(listp_t));
 
-		if (new == NULL)
-			exit(98);
+if (new == NULL)
+exit(98);
 
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
+new->p = (void *)head;
+new->next = hptr;
+hptr = new;
 
-		add = hptr;
+add = hptr;
 
-		while (add->next != NULL)
-		{
-			add = add->next;
-			if (head == add->p)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
-				return (nnodes);
+while (add->next != NULL)
+{
+add = add->next;
+if (head == add->p)
+{
+printf("-> [%p] %d\n", (void *)head, head->n);
+free_listp(&hptr);
+return (nnodes);
 }
 }
 printf("[%p] %d\n", (void *)head, head->n);
